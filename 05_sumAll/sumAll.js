7258498260bba4c8345f9596b95e7b2d63a33aaa
ajay91py <166129570+ajay91py@users.noreply.github.com>
2024-05-21 13:21:04 +0530

    exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(mn, mx) {
+    if (!Number.isInteger(mn) || !Number.isInteger(mx)) return "ERROR";
+    if (mn < 0 || mx < 0) return "ERROR";
+    if (mn > mx) {
+      const temp = mn;
+      mn = mx;
+      mx = temp;
 };
 
+let sum=0;
+for (let i=mn; i<=mx; i++)
+    {
+     sum+= i;
+    }
+    return sum;}
 // Do not edit below this line
 module.exports = sumAll;

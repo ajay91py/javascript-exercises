ce31c1127a41bc4ba3b06d79b4f9132bb94b0e93
ajay91py <166129570+ajay91py@users.noreply.github.com>
2024-05-21 11:27:24 +0530

    exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let word='';
 
-};
+if(num<0) {
+    word='ERROR'
+}
+    
+    for(let i=0; i<num; i++)
+{
+   word +=string;
+}
+return word; 
+}; 
+ 
 
 // Do not edit below this line
 module.exports = repeatString;
